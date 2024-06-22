294f84b02b1d87eff4b336da634b71187fc201d7
Tania <tanner.shirkey@gmail.com>
2024-06-22 16:43:12 -0400

    added landing page as about page
    about me page will act as the landing page for the site. Also added page routing and beginning of overall site layout with header and navigation bar.

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+// Stand-in for the "Contact" page.
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>
+        Add more later!
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,11 @@
+// Stand-in for the "Portfolio" page.
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>My Work</h1>
+      <p>
+        Add more later!
+      </p>
+    </div>
+  );
+}
