4dba4e6661ff7fdd482bd4de95d0e1d1c8af4a58
Tania <tanner.shirkey@gmail.com>
2024-06-23 16:49:37 -0400

    added contact page
    contact page will take in your name, email, and a message and pass it through a submit event. Submit isn't fully done yet, the form will not do anything with this data yet. Each field will check if they are completed, and the email field will check for invalid emails.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,35 @@
 // Stand-in for the "Contact" page.
+import { useId } from 'react';
+
 export default function Contact() {
+  const textAreaId = useId();
+
   return (
     <div>
       <h1>Contact Me</h1>
       <p>
-        Add more later!
+        Howdy! If you wish to contact me, just leave your name, return email,
+        and message in the input fields below. I&apos;ll be sure to message you as
+        soon as I am able.
       </p>
+      <form>
+        <label htmlFor="nameInput">Your Name</label><br />
+        <input name="nameInput" type="text" required /><br />
+
+        <label htmlFor="emailInput">Your Email</label><br />
+        <input name="emailInput" type="email" required /><br />
+
+        <label htmlFor={textAreaId}>Your Message</label><br />
+        <textarea
+          id={textAreaId}
+          name="messageInput"
+          rows={4}
+          cols={40}
+          required
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
