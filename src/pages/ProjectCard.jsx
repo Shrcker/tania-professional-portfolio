82ca50886a07573473e6c591e3419fc64c25bf9a
Tania <tanner.shirkey@gmail.com>
2024-06-24 16:07:28 -0400

    added project image size
    constrained image size of portofolio project previews so that they don't take up as much of the screen. Will need to replace with cropped images later, but should work for now

--- src/pages/ProjectCard.jsx
+++ src/pages/ProjectCard.jsx
@@ -1,9 +1,10 @@
 export default function ProjectCard({ project }) {
   const { title, description, image, deployLink, gitLink } = project;
+  const imageSize = [579, 270];
 
   return (
     <div className="card">
-      <img src={image} alt={title}/>
+      <img src={image} alt={title} width={imageSize[0]} height={imageSize[1]} />
       <div className="cardBody">
         <h5>{title}</h5>
         <p>{description}</p>
